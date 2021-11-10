c2a079b39ffc46b95f5f980c1f7d27eda5abd61e
drew-sys <90053966+drew-sys@users.noreply.github.com>
2021-11-10 20:09:01 +0000

    added data pickling and extraction functions to utils

--- data_import.py
+++ data_import.py
@@ -4,35 +4,13 @@ import pickle
 import pandas as pd
 
 # For logger and units dict
-from utils import get_logger, read_pickle_folder
+from utils import get_logger, read_pickle_folder, extract_data, serialize_df_dict
 
 # Get model parameters
 from model_config import IMPORT_DATA_PATH, PKL_FOLDER
 
 # Create logger
 logger = get_logger('Data Import')
-
-def extract_data(data_path: str, filename: str, ext: str, sheet: int=0) -> pd.DataFrame:
-    """Extracts data from excel or csv files based on input parameters
-
-    Args:
-        data_path (str): path where data files are stored
-        filename (str): name of file to extract (without extension)
-        ext (str): extension of the file to extract
-        sheet (int, optional): Number of the sheet to extract. For xlsx (workbook) files only. - . Defaults to 0.
-
-    Returns:
-        DataFrame: A dataframe of the data file
-    """
-    # Full path of the file
-    full_filename = fr'{data_path}/{filename}.{ext}'
-    # If else logic that determines which pandas function to call based on the extension
-    logger.info(f'|| Extracting file {filename}.{ext}')
-    if ext == 'xlsx':
-        return pd.read_excel(full_filename, sheet_name=sheet)
-    elif ext == 'csv':
-        return pd.read_csv(full_filename)
-
 
 def replace_rows(df: pd.DataFrame, header_row: int) -> pd.DataFrame:
     """For WSA trade data, this function replaces the column names with the appropriate row.
@@ -49,20 +27,6 @@ def replace_rows(df: pd.DataFrame, header_row: int) -> pd.DataFrame:
     df_c = df[header_row+1:] # take the data less the header row
     df_c.columns = new_header # set the header row as the df header
     return df_c
-
-def df_serializer(data_path: str, data_dict: dict):
-    """Iterate through each df and store the file as pickle or feather. Does not return any object.
-
-    Args:
-        data_ref (dict): A data dictionary where the DataFrames are stored
-        data_path (str): The path where the pickle files will be stored
-    """
-    logger.info(f'||| Serializing each df to a pickle file {data_path}')
-    for df_name in data_dict.keys():
-        with open(f'{data_path}/{df_name}.pickle', 'wb') as f:
-            # Pickle the 'data' dictionary using the highest protocol available.
-            logger.info(f'* Saving df {df_name} to pickle')
-            pickle.dump(data_dict[df_name], f, pickle.HIGHEST_PROTOCOL)
 
 # Import capex numbers
 greenfield_capex = extract_data(IMPORT_DATA_PATH, "CAPEX OPEX Per Technology", "xlsx", 0)
@@ -98,6 +62,9 @@ steel_plants = extract_data(IMPORT_DATA_PATH, "Steel Plant Data", "csv")
 
 # Import technology availability
 tech_availability = extract_data(IMPORT_DATA_PATH, "Technology Availability", "csv")
+
+# Import power grid assumptions
+power_grid_assumptions = extract_data(IMPORT_DATA_PATH, 'Power Grid Assumptions', 'xlsx')
 
 # Import WSA data
 crude_regional_shares = extract_data(IMPORT_DATA_PATH, "WSA World Steel In Figures 2021", "xlsx", 0)
@@ -135,11 +102,12 @@ df_dict = {
     "iron_ore_trade" : iron_ore_trade,
     "scrap_trade" : scrap_trade,
     "s3_emissions_factors" : s3_emissions_factors,
-    "business_cases": business_cases
+    "business_cases": business_cases,
+    "power_grid_assumptions": power_grid_assumptions,
 }
 
 # Turn dataframes into pickle files
-df_serializer(PKL_FOLDER, df_dict)
+serialize_df_dict(PKL_FOLDER, df_dict)
 
 # Unload dataframes from pickle files
 pkl_dict = read_pickle_folder(PKL_FOLDER)
--- utils.py
+++ utils.py
@@ -4,6 +4,8 @@ import pickle
 import sys
 import os
 
+import pandas as pd
+
 from logging.handlers import TimedRotatingFileHandler
 
 FORMATTER = logging.Formatter("%(asctime)s — %(name)s — %(levelname)s — %(message)s")
@@ -67,3 +69,45 @@ def read_pickle_folder(data_path: str):
             new_data_dict[pkl_file.split('.')[0]] = pickle.load(f)
 
     return new_data_dict
+
+
+def extract_data(data_path: str, filename: str, ext: str, sheet: int=0) -> pd.DataFrame:
+    """Extracts data from excel or csv files based on input parameters
+
+    Args:
+        data_path (str): path where data files are stored
+        filename (str): name of file to extract (without extension)
+        ext (str): extension of the file to extract
+        sheet (int, optional): Number of the sheet to extract. For xlsx (workbook) files only. - . Defaults to 0.
+
+    Returns:
+        DataFrame: A dataframe of the data file
+    """
+    # Full path of the file
+    full_filename = fr'{data_path}/{filename}.{ext}'
+    # If else logic that determines which pandas function to call based on the extension
+    logger.info(f'|| Extracting file {filename}.{ext}')
+    if ext == 'xlsx':
+        return pd.read_excel(full_filename, sheet_name=sheet)
+    elif ext == 'csv':
+        return pd.read_csv(full_filename)
+
+def serialize_df(df: pd.DataFrame, data_path: str, filename: str):
+    with open(f'{data_path}/{filename}.pickle', 'wb') as f:
+        # Pickle the 'data' dictionary using the highest protocol available.
+        logger.info(f'* Saving df {filename} to pickle')
+        pickle.dump(df, f, pickle.HIGHEST_PROTOCOL)
+
+def serialize_df_dict(data_path: str, data_dict: dict):
+    """Iterate through each df and store the file as pickle or feather. Does not return any object.
+
+    Args:
+        data_ref (dict): A data dictionary where the DataFrames are stored
+        data_path (str): The path where the pickle files will be stored
+    """
+    logger.info(f'||| Serializing each df to a pickle file {data_path}')
+    for df_name in data_dict.keys():
+        with open(f'{data_path}/{df_name}.pickle', 'wb') as f:
+            # Pickle the 'data' dictionary using the highest protocol available.
+            logger.info(f'* Saving df {df_name} to pickle')
+            pickle.dump(data_dict[df_name], f, pickle.HIGHEST_PROTOCOL)
