954265a28b2d697079b78534cb1da0f878e2735d
drew-sys <90053966+drew-sys@users.noreply.github.com>
2021-11-12 17:58:37 +0000

    added data tuple creator for feedstock prices

--- data_interface.py
+++ data_interface.py
@@ -1,4 +1,7 @@
 """Functions to access data sources"""
+
+from collections import namedtuple
+
 # For Data Manipulation
 import pandas as pd
 
@@ -11,7 +14,7 @@ from model_config import PKL_FOLDER
 # Create logger
 logger = get_logger('Data Interface')
 
-def capex_melt_and_format(df: pd.DataFrame) -> pd.DataFrame:
+def capex_melt_and_index(df: pd.DataFrame) -> pd.DataFrame:
     """Make the capex dataframes tabular and create a multiindex
 
     Args:
@@ -49,10 +52,31 @@ def capex_generator(
         'brownfield': brownfield,
         'other_opex': other_opex
     }
-
+    
     if output_type == 'all':
+        logger.info(f'Creating capex values dictionary')
         return capex_dict
+    logger.info(f'Creating capex value')
     return capex_dict[output_type]
+
+def create_data_tuples(df: pd.DataFrame, namedtuple_name: str):
+    """Generic function that dynamically create namedtuples from a dataframe.
+    *** WARNING *** You won't see these namedtuples defined prior to creation so you have to be careful.
+
+    Args:
+        df (pd.DataFrame): DataFrame containing the data to translate into the namedtuples
+        namedtuple_name (str): The name of the namedtuple that you want to create
+    """
+    df_c = df.copy()
+    df_c.columns = [colname.lower() for colname in df_c.columns]
+    class_names = [metric.upper().replace(' ', '_') for metric in df_c['metric'].to_list()]
+    globals()[namedtuple_name] = namedtuple('namedtuple_name', ['metric', 'unit', 'year', 'value'])
+    ticker = 0
+    logger.info(f'Creating new namtedtuples with the following names: {class_names}')
+    for row in df_c.itertuples():
+        globals()[class_names[ticker]] = globals()[namedtuple_name](row.metric, row.unit, row.year, row.value)
+        ticker+=1
+
 
 greenfield_capex_df = read_pickle_folder(PKL_FOLDER, 'greenfield_capex')
 brownfield_capex_df = read_pickle_folder(PKL_FOLDER, 'brownfield_capex')
@@ -62,11 +86,13 @@ brownfield_capex_df.drop(
     ['Available from', 'Available until', 'Technology type'], axis=1, inplace=True)
 
 capex_dictionary = {
-    'greenfield': capex_melt_and_format(greenfield_capex_df),
-    'brownfield': capex_melt_and_format(brownfield_capex_df),
-    'other_opex': capex_melt_and_format(other_opex_df)
+    'greenfield': capex_melt_and_index(greenfield_capex_df),
+    'brownfield': capex_melt_and_index(brownfield_capex_df),
+    'other_opex': capex_melt_and_index(other_opex_df)
 }
 
 example_result = capex_generator(capex_dictionary, 'DRI-EAF', 2025)
 
-print(example_result)
+feedstock_prices = read_pickle_folder(PKL_FOLDER, 'feedstock_prices')
+
+create_data_tuples(feedstock_prices, 'FeedstockPrices')
