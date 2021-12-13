be0cedc7208bd2e736bb56181c06ff9cbf1c898c
Luis Natera <natera@hey.com>
2021-12-13 16:41:56 +0000

    Capex_data_points to model_config.py

--- mppsteel/model_config.py
+++ mppsteel/model_config.py
@@ -228,3 +228,9 @@ SWITCH_DICT = {
     "Electrolyzer-EAF": ["Electrolyzer-EAF"],
     "Electrowinning-EAF": ["Electrowinning-EAF"],
 }
+
+CAPEX_DATA_POINTS = {
+    "2020": 319.249187119815,
+    "2030": 319.249187119815,
+    "2050": 286.218839300307,
+}
