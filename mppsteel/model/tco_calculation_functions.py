a16e3a83561d89ba4d971c982d76f6bb4a304db4
drew-sys <90053966+drew-sys@users.noreply.github.com>
2022-01-29 00:40:10 +0000

    removed non-tco calculations from tco calcs

--- mppsteel/model/tco_calculation_functions.py
+++ mppsteel/model/tco_calculation_functions.py
@@ -1,4 +1,4 @@
-"""Main solving script for deciding investment decisions."""
+"""TCO Calculations used to derive the Total Cost of Ownership"""
 
 from functools import lru_cache
 
@@ -9,28 +9,24 @@ from tqdm import tqdm
 from tqdm.auto import tqdm as tqdma
 
 from mppsteel.model.emissions_reference_tables import get_s2_emissions
-from mppsteel.model.financial_functions import generate_capex_financial_summary
 
 from mppsteel.utility.utils import (
-    read_pickle_folder, get_logger,
-    enumerate_columns,
+    get_logger, enumerate_columns,
 )
 from mppsteel.model_config import (
-    MODEL_YEAR_END, PKL_DATA_INTERMEDIATE, DISCOUNT_RATE,
-    INVESTMENT_CYCLE_LENGTH, STEEL_PLANT_LIFETIME
+    MODEL_YEAR_END, DISCOUNT_RATE,
 )
 from mppsteel.utility.reference_lists import (
     SWITCH_DICT
 )
 
-# Create logger
-logger = get_logger("TCO")
+logger = get_logger("TCO Calculation Functions")
 
 def carbon_tax_estimate(s1_emissions_value: dict, scope2_emission_value: float, carbon_tax_value: pd.DataFrame):
     return (scope2_emission_value + s1_emissions_value) * carbon_tax_value
 
 @lru_cache(maxsize=200000)
-def get_steel_making_cost(variable_tech_cost: float, primary_capacity: float, secondary_capacity: float, technology: str, eur_usd_rate: float):
+def green_premium_capacity_calculation(variable_tech_cost: float, primary_capacity: float, secondary_capacity: float, technology: str, eur_usd_rate: float):
     combined_capacity = primary_capacity + secondary_capacity
     if technology == 'EAF':
         variable_cost_value = (variable_tech_cost * primary_capacity) + (variable_tech_cost * secondary_capacity)
@@ -38,14 +34,13 @@ def get_steel_making_cost(variable_tech_cost: float, primary_capacity: float, se
         variable_cost_value = variable_tech_cost * primary_capacity
     return combined_capacity * (variable_cost_value / combined_capacity) / combined_capacity / eur_usd_rate
 
-
 def calculate_green_premium(variable_costs, steel_plant_df, green_premium_timeseries, country_code, plant_name, technology_2020, year, eur_usd_rate: float):
     variable_tech_cost = variable_costs.loc[country_code, year, technology_2020].values[0]
     steel_plant_df_c = steel_plant_df.loc[steel_plant_df['plant_name'] == plant_name].copy()
     primary_capacity = steel_plant_df_c['primary_capacity_2020'].values[0]
     secondary_capacity = steel_plant_df_c['secondary_capacity_2020'].values[0]
     green_premium = green_premium_timeseries.loc[green_premium_timeseries['year'] == year]['value']
-    steel_making_cost = get_steel_making_cost(variable_tech_cost, primary_capacity, secondary_capacity, technology_2020, eur_usd_rate)
+    steel_making_cost = green_premium_capacity_calculation(variable_tech_cost, primary_capacity, secondary_capacity, technology_2020, eur_usd_rate)
     return steel_making_cost * green_premium
 
 def get_opex_costs(
@@ -158,36 +153,3 @@ def tco_calc(
     column_order = ['country_code', 'year', 'start_technology', 'end_technology', 'capex_value', 'discounted_opex']
     capex_opex_values.reset_index(inplace=True)
     return capex_opex_values[column_order]
-
-def capex_values_for_levelised_steelmaking(capex_df: pd.DataFrame, int_rate: float, year: int, payments: int):
-    df_temp = capex_df.swaplevel().loc[year]
-    value_list = []
-    def value_mapper(row, enum_dict):
-        capex_value = - generate_capex_financial_summary(row[enum_dict['value']], int_rate, payments)['total_interest']
-        value_list.append(capex_value)
-    tqdma.pandas(desc="Capex Values for Levelised Steel")
-    enumerated_cols = enumerate_columns(df_temp.columns)
-    df_temp.progress_apply(value_mapper, enum_dict=enumerated_cols, axis=1, raw=True)
-    df_temp.drop(['value'], axis=1, inplace=True)
-    df_temp['value'] = value_list
-    return df_temp
-
-def levelised_steelmaking_cost(year: pd.DataFrame, include_greenfield: bool = False):
-    # Levelised of cost of steelmaking = OtherOpex + VariableOpex + RenovationCapex w/ 7% over 20 years (+ GreenfieldCapex w/ 7% over 40 years)
-    df_list = []
-    variable_costs_regional = read_pickle_folder(PKL_DATA_INTERMEDIATE, 'variable_costs_regional', 'df')
-    opex_values_dict = read_pickle_folder(PKL_DATA_INTERMEDIATE, 'capex_dict', 'df')
-    for plant_country_ref in variable_costs_regional.index.get_level_values(0).unique():
-        variable_costs = variable_costs_regional.loc[plant_country_ref, year]
-        other_opex = opex_values_dict['other_opex'].swaplevel().loc[year]
-        brownfield_capex = capex_values_for_levelised_steelmaking(opex_values_dict['brownfield'], DISCOUNT_RATE, year, INVESTMENT_CYCLE_LENGTH)
-        variable_costs.rename(mapper={'cost': 'value'}, axis=1, inplace=True)
-        variable_costs.rename(mapper={'technology': 'Technology'}, axis=0, inplace=True)
-        combined_df = variable_costs + other_opex + brownfield_capex
-        if include_greenfield:
-            greenfield_capex = capex_values_for_levelised_steelmaking(opex_values_dict['greenfield'], DISCOUNT_RATE, year, STEEL_PLANT_LIFETIME)
-            combined_df = variable_costs + other_opex + greenfield_capex + brownfield_capex
-        combined_df['plant_country_ref'] = plant_country_ref
-        df_list.append(combined_df)
-    df = pd.concat(df_list)
-    return df.reset_index().rename(mapper={'index': 'technology'},axis=1).set_index(['plant_country_ref', 'technology'])
