581832c5baac92692b47d51178081cdefef821de
drew-sys <90053966+drew-sys@users.noreply.github.com>
2022-02-03 15:13:13 +0000

    new pandera function updates

--- mppsteel/validation/data_import_tests.py
+++ mppsteel/validation/data_import_tests.py
@@ -4,40 +4,46 @@ import pandera as pa
 from pandera import (
     DataFrameSchema, Column, Index, MultiIndex, Check
 )
-from pandera.typing import Index, DataFrame, Series
+from pandera.typing import DataFrame, Series
 
 from mppsteel.utility.file_handling_utility import read_pickle_folder
 from mppsteel.model_config import PKL_DATA_IMPORTS
+from mppsteel.utility.reference_lists import TECH_REFERENCE_LIST
 
 # Strict w/ filter
 # Transforming Schema: add_columns() , remove_columns(), update_columns(), rename_columns(), set_index(), and reset_index()
+# You can create dummy data from a schema schema.example(size=x)
+# pa.schema_inference.infer_schema(steel_plants))
+
+TECH_REFERENCE_LIST_ADJ = TECH_REFERENCE_LIST.copy()
+TECH_REFERENCE_LIST_ADJ += ['Charcoal mini furnace', 'Close plant', 'New capacity']
 
 RE_YEAR_COL_TEST = '^[12][0-9]{3}$'
 YEAR_VALUE_TEST = Column(int, Check.greater_than_or_equal_to(2020))
-COUNTRY_CODE_CHECK = Column(str, Check.str_length(8), required=True)
+COUNTRY_CODE_CHECK = Column(str, Check.str_length(3), required=True, nullable=True)
 NULLABLE_INT_CHECK = Column(int, nullable=True)
 NULLABLE_STR_CHECK = Column(str, nullable=True)
 UNIT_COL_TEST = Column(str, Check.str_contains('/'))
+VALUE_COL_TEST = Column(float, nullable=False)
+COERCE_TO_STRING = Column(str, nullable=True, coerce=True)
 
-# OBJECT METHOD
 COUNTRY_REF_SCHEMA = DataFrameSchema({
     'Country': Column(str),
-    'ISO-alpha3 code': COUNTRY_CODE_CHECK,
-    'Region 1': Column(str),
+    'ISO-alpha3 code': Column(str, Check.str_length(3), required=True, nullable=True),
     'M49 Code': Column(int),
+    'Region 1': Column(str),
     'Continent': Column(str),
     'WSA Group Region': Column(str),
     'RMI Model Region': Column(str),
 })
 
-# CLASS METHOD
-class FeedstockInputSchema(pa.SchemaModel):
-    Year: Series[int] = pa.Field(ge=2020, coerce=True)
-    Category: Series[str] = pa.Field()
-    Value: Series[float] = pa.Field()
-    Unit: Series[str] = pa.Field()
-    Source: Series[str] = pa.Field()
-
+FEEDSTOCK_INPUT_SCHEMA = DataFrameSchema({
+    'Year': Column(int, Check.greater_than_or_equal_to(2020)),
+    'Category': Column(str),
+    'Unit': UNIT_COL_TEST,
+    'Value': VALUE_COL_TEST,
+    'Source': Column(str, nullable=True, coerce=True),
+})
 
 STEEL_PLANT_DATA_SCHEMA = DataFrameSchema(
     columns={
@@ -48,7 +54,7 @@ STEEL_PLANT_DATA_SCHEMA = DataFrameSchema(
         'Region - analysis': Column(str),
         'Coordinates': Column(str),
         'Status': Column(str),
-        'Start of operation': Column(int, coerce=True),
+        'Start of operation': Column(str, nullable=True, coerce=True), # WARNING! handle this case later
         'Fill in data BF-BOF': Column(float),
         'Fill in data EAF': Column(float),
         'Fill in data DRI': Column(float),
@@ -71,45 +77,45 @@ STEEL_PLANT_DATA_SCHEMA = DataFrameSchema(
 ETHANOL_PLASTIC_CHARCOAL_SCHEMA = DataFrameSchema(
     columns={
         'Classification': Column(str, Check.str_length(2)),
-        'Year': YEAR_VALUE_TEST,
-        'Period': YEAR_VALUE_TEST,
-        'Period Desc.': YEAR_VALUE_TEST,
+        'Year': Column(int, Check.greater_than_or_equal_to(2020)),
+        'Period': Column(int, Check.greater_than_or_equal_to(2020)),
+        'Period Desc.': Column(int, Check.greater_than_or_equal_to(2020)),
         'Aggregate Level': Column(int, Check.less_than(10)),
         'Is Leaf Code': Column(int, Check.less_than(2)),
-        'Trade Flow Code': Column(int, Check.less_than(4)),
-        'Trade Flow': Column(str, Check.isin(['Import', 'Export'])),
+        'Trade Flow Code': Column(int, Check.less_than(5)),
+        'Trade Flow': Column(str, Check.isin(['Import', 'Export', 'Re-Export', 'Re-Import'])),
         'Reporter Code': Column(int),
         'Reporter': Column(str),
         'Reporter ISO': COUNTRY_CODE_CHECK,
         'Partner Code': Column(int),
         'Partner': Column(str, Check.str_matches('World')),
         'Partner ISO': Column(str, Check.str_matches('WLD')),
-        '2nd Partner Code': Column(str, nullable=True),
-        '2nd Partner': Column(str, nullable=True),
-        '2nd Partner ISO': Column(str, nullable=True),
-        'Customs Proc. Code': Column(str, nullable=True),
-        'Customs': Column(str, nullable=True),
-        'Mode of Transport Code': NULLABLE_STR_CHECK,
-        'Mode of Transport': NULLABLE_STR_CHECK,
-        'Commodity Code': NULLABLE_STR_CHECK,
-        'Commodity': NULLABLE_INT_CHECK,
-        'Qty Unit Code': NULLABLE_INT_CHECK,
-        'Qty Unit': NULLABLE_INT_CHECK,
-        'Qty':  NULLABLE_INT_CHECK,
-        'Alt Qty Unit Code': NULLABLE_INT_CHECK,
-        'Alt Qty Unit': NULLABLE_INT_CHECK,
-        'Alt Qty': NULLABLE_INT_CHECK,
-        'Netweight (kg)': NULLABLE_INT_CHECK,
-        'Gross weight (kg)': NULLABLE_INT_CHECK,
-        'Trade Value (US$)': NULLABLE_INT_CHECK,
-        'CIF Trade Value (US$)': NULLABLE_INT_CHECK,
-        'FOB Trade Value (US$)': NULLABLE_INT_CHECK,
-        'Flag': NULLABLE_INT_CHECK,
+        '2nd Partner Code': Column(str, nullable=True, coerce=True),
+        '2nd Partner': Column(str, nullable=True, coerce=True),
+        '2nd Partner ISO': Column(str, nullable=True, coerce=True),
+        'Customs Proc. Code': Column(str, nullable=True, coerce=True),
+        'Customs': Column(str, nullable=True, coerce=True),
+        'Mode of Transport Code': Column(str, nullable=True, coerce=True),
+        'Mode of Transport': Column(str, nullable=True, coerce=True),
+        'Commodity Code': Column(int, nullable=True),
+        'Commodity': Column(str, nullable=True),
+        'Qty Unit Code': Column(int, nullable=True),
+        'Qty Unit': Column(str, nullable=True),
+        'Qty':  Column(int, nullable=True),
+        'Alt Qty Unit Code': Column(str, nullable=True, coerce=True),
+        'Alt Qty Unit': Column(str, nullable=True, coerce=True),
+        'Alt Qty': Column(float, nullable=True),
+        'Netweight (kg)': Column(float, nullable=True, coerce=True),
+        'Gross weight (kg)': Column(float, nullable=True, coerce=True),
+        'Trade Value (US$)': Column(float, nullable=True, coerce=True),
+        'CIF Trade Value (US$)': Column(float, nullable=True, coerce=True),
+        'FOB Trade Value (US$)': Column(float, nullable=True, coerce=True),
+        'Flag': Column(int, nullable=True),
     }
 )
 
 CAPEX_OPEX_PER_TECH_SCHEMA = DataFrameSchema({
-    'Technology': Column(str),
+    'Technology': Column(str, Check.isin(TECH_REFERENCE_LIST_ADJ)),
     RE_YEAR_COL_TEST: Column(float, regex=True),
 })
 
@@ -123,7 +129,7 @@ REGIONAL_STEEL_DEMAND_SCHEMA = DataFrameSchema({
 SCOPE3_EF_SCHEMA_1 = DataFrameSchema({
     'Category': Column(str),
     'Fuel': Column(str),
-    'Unit': Column(str, Check.str_contains('/')),
+    'Unit': UNIT_COL_TEST,
     RE_YEAR_COL_TEST: Column(float, regex=True),
 })
 
@@ -133,28 +139,46 @@ SCOPE3_EF_SCHEMA_2 = DataFrameSchema({
 })
 
 SCOPE1_EF_SCHEMA = DataFrameSchema({
-    'Year': YEAR_VALUE_TEST,
+    'Year': Column(int, Check.greater_than_or_equal_to(2020)),
     'Category': Column(str),
     'Metric': Column(str),
     'Unit': UNIT_COL_TEST,
-    'Value': Column(float),
+    'Value': VALUE_COL_TEST,
+    'Source': Column(str, nullable=True, coerce=True),
+})
+
+ENERGY_PRICES_STATIC_SCHEMA = DataFrameSchema({
+    'Year': Column(int, Check.greater_than_or_equal_to(2020)),
+    'Category': Column(str),
+    'Metric': Column(str),
+    'Value': VALUE_COL_TEST,
     'Source': Column(str)
 })
 
-ENERGY_PRICES_STATIC = DataFrameSchema({
-    'Year': YEAR_VALUE_TEST,
-    'Category': Column(str),
-    'Metric': Column(str),
-    'Value': Column(float),
-    'Source': Column(str)
+TECH_AVAILABILIY_SCHEMA = DataFrameSchema({
+    'Technology': Column(str, Check.isin(TECH_REFERENCE_LIST_ADJ)),
+    'Main Technology Type': Column(str),
+    'Technology Phase': Column(str),
+    'Year available from': Column(int, Check.greater_than_or_equal_to(2020)),
+    'Year available until': Column(int, Check.greater_than_or_equal_to(2020)),
+    'Source': Column(str, nullable=True, coerce=True),
 })
 
-TECH_AVAILABILIY = DataFrameSchema({
-    'Year': YEAR_VALUE_TEST,
-    'Category': Column(str),
+CO2_CCS_SCHEMA = DataFrameSchema({
     'Metric': Column(str),
-    'Value': Column(float),
-    'Source': Column(str)
+    'Year': Column(int, Check.greater_than_or_equal_to(2020)),
+    'Units': Column(str),
+    'Value': VALUE_COL_TEST,
+    'Source': Column(str),
+})
+
+STEEL_BUSINESS_CASES_SCHEMA = DataFrameSchema({
+    'Section': Column(str),
+    'Process': Column(str),
+    'Process Detail': Column(str),
+    'Step': Column(str),
+    'Material Category': Column(str),
+    'Unit': Column(str),
 })
 
 def import_data_tests():
@@ -162,10 +186,10 @@ def import_data_tests():
     COUNTRY_REF_SCHEMA.validate(country_ref)
 
     feedstock_prices = read_pickle_folder(PKL_DATA_IMPORTS, "feedstock_prices")
-    FeedstockInputSchema.validate(feedstock_prices)
+    FEEDSTOCK_INPUT_SCHEMA.validate(feedstock_prices)
 
     steel_plants = read_pickle_folder(PKL_DATA_IMPORTS, "steel_plants")
-    # STEEL_PLANT_DATA_SCHEMA.validate(steel_plants) # will fail due to 'anticipated and NaNs'
+    STEEL_PLANT_DATA_SCHEMA.validate(steel_plants) # will fail due to 'anticipated and NaNs'
 
     greenfield_capex = read_pickle_folder(PKL_DATA_IMPORTS, "greenfield_capex")
     brownfield_capex = read_pickle_folder(PKL_DATA_IMPORTS, "brownfield_capex")
@@ -173,7 +197,6 @@ def import_data_tests():
 
     for capex_df in [greenfield_capex, brownfield_capex, other_opex]:
         CAPEX_OPEX_PER_TECH_SCHEMA.validate(capex_df)
-
 
     s3_emissions_factors_1 = read_pickle_folder(PKL_DATA_IMPORTS, "s3_emissions_factors_1")
     SCOPE3_EF_SCHEMA_1.validate(s3_emissions_factors_1)
@@ -188,8 +211,16 @@ def import_data_tests():
     SCOPE1_EF_SCHEMA.validate(s1_emissions_factors)
 
     static_energy_prices = read_pickle_folder(PKL_DATA_IMPORTS, "static_energy_prices")
-    ENERGY_PRICES_STATIC.validate(static_energy_prices)
+    ENERGY_PRICES_STATIC_SCHEMA.validate(static_energy_prices)
 
     tech_availability = read_pickle_folder(PKL_DATA_IMPORTS, "tech_availability")
+    TECH_AVAILABILIY_SCHEMA.validate(tech_availability)
 
-    # (pa.schema_inference.infer_schema(steel_plants))
+    regional_steel_demand = read_pickle_folder(PKL_DATA_IMPORTS, "regional_steel_demand")
+    REGIONAL_STEEL_DEMAND_SCHEMA.validate(regional_steel_demand)
+
+    co2_ccs = read_pickle_folder(PKL_DATA_IMPORTS, "ccs_co2")
+    CO2_CCS_SCHEMA.validate(co2_ccs)
+
+    business_cases = read_pickle_folder(PKL_DATA_IMPORTS, "business_cases")
+    STEEL_BUSINESS_CASES_SCHEMA.validate(business_cases)
