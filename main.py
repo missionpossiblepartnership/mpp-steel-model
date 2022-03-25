fbb13c7adbe66cc3e990f93e4078d20b1cc3ef84
drew-sys <90053966+drew-sys@users.noreply.github.com>
2022-03-25 16:48:30 +0000

    added investment cycles as a model run

--- main.py
+++ main.py
@@ -122,7 +122,7 @@ if __name__ == "__main__":
     if args.emissivity:
         generate_emissions_flow(scenario_dict=scenario_args, serialize=True)
 
-    if args.green_capacity_ratio:
-        gcr_flow(scenario_dict=scenario_args)
+    if args.investment_cycles:
+        investment_cycles(scenario_dict=scenario_args)
 
     TIME_CONTAINER.return_time_container()
