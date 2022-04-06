16c13fc2a9067dfb52e83b4d5c16e2a56ded2394
drew-sys <90053966+drew-sys@users.noreply.github.com>
2022-04-06 13:09:09 +0000

    optimized the investments script runtime

--- mppsteel/results/investments.py
+++ mppsteel/results/investments.py
@@ -1,4 +1,6 @@
 """Investment Results generator for technology investments"""
+
+import itertools
 from typing import Union
 
 import pandas as pd
@@ -7,8 +9,10 @@ from tqdm import tqdm
 from mppsteel.config.model_config import (
     MODEL_YEAR_START,
     MODEL_YEAR_END,
+    PKL_DATA_IMPORTS,
     PKL_DATA_FORMATTED
 )
+from mppsteel.config.reference_lists import SWITCH_DICT
 
 from mppsteel.utility.function_timer_utility import timer_func
 from mppsteel.utility.dataframe_utility import add_results_metadata
@@ -18,6 +22,7 @@ from mppsteel.utility.file_handling_utility import (
 from mppsteel.utility.log_utility import get_logger
 from mppsteel.data_loading.steel_plant_formatter import map_plant_id_to_df
 from mppsteel.results.production import get_tech_choice, production_stats_getter
+from mppsteel.utility.location_utility import create_country_mapper
 
 # Create logger
 logger = get_logger(__name__)
@@ -51,14 +56,9 @@ def capex_getter_f(
     Returns:
         float: A value containing the capex value based on the function arguments.
     """
-    capex_year = min(MODEL_YEAR_END, year)
-    if not start_tech:
+    if not start_tech or (new_tech == "Close plant") or (switch_type == "no switch"):
         return 0
-    if new_tech == "Close plant":
-        return 0
-    if switch_type == "no switch":
-        return 0
-    capex_ref = capex_df.loc[capex_year, start_tech]
+    capex_ref = capex_df.loc[year, start_tech]
     return capex_ref.loc[capex_ref["new_technology"] == new_tech]["value"].values[0]
 
 
@@ -75,20 +75,16 @@ def investment_switch_getter(
     Returns:
         str: The switch type [`no switch`, `trans switch`, `main cycle`].
     """
-    inv_df_ref = (
-        inv_df.reset_index().set_index(["year", "plant_name"]).sort_values(["year"])
-    )
     return inv_df_ref.loc[year, plant_name].values[0]
 
 
 def investment_row_calculator(
-    inv_df: pd.DataFrame,
+    investment_switch_ref: dict,
     capex_df: pd.DataFrame,
     tech_choices: dict,
-    plant_name: str,
-    country_code: str,
+    capacity_ref: dict,
     year: int,
-    capacity_value: float,
+    plant_name: str
 ) -> dict:
     """_summary_
 
@@ -104,7 +100,7 @@ def investment_row_calculator(
     Returns:
         dict: A dictionary containing the column-value pairs to be inserted in a DataFrame.
     """
-    switch_type = investment_switch_getter(inv_df, year, plant_name)
+    switch_type = investment_switch_ref[(year, plant_name)]
 
     if year == 2020:
         start_tech = get_tech_choice(tech_choices, 2020, plant_name)
@@ -114,11 +110,10 @@ def investment_row_calculator(
     new_tech = get_tech_choice(tech_choices, year, plant_name)
     actual_capex = 0
     if new_tech:
-        capex_ref = capex_getter_f(capex_df, year, start_tech, new_tech, switch_type)
-        actual_capex = capex_ref * (capacity_value * 1000 * 1000)  # convert from Mt to T
+        capex_value = capex_getter_f(capex_df, year, start_tech, new_tech, switch_type)
+        actual_capex = capex_value * (capacity_ref[plant_name] * 1000 * 1000)  # convert from Mt to T
     return {
         "plant_name": plant_name,
-        "country_code": country_code,
         "year": year,
         "start_tech": start_tech,
         "end_tech": new_tech,
@@ -151,7 +146,7 @@ def create_inv_stats(
             "end_tech",
             "switch_type",
             "capital_cost",
-            "region_wsa_region",
+            "rmi_region",
         ]
     ].copy()
 
@@ -166,10 +161,10 @@ def create_inv_stats(
         return create_global_stats(df_c, operation).reset_index()
 
     if results == "regional":
-        regions = df_c["region_wsa_region"].unique()
+        regions = df_c["rmi_region"].unique()
         region_dict = {}
         for region in regions:
-            calc = df_c[df_c["region_wsa_region"] == region].groupby(["year"]).sum()
+            calc = df_c[df_c["rmi_region"] == region].groupby(["year"]).sum()
             if operation == "sum":
                 pass
             if operation == "cumsum":
@@ -240,43 +235,62 @@ def investment_results(scenario_dict: dict, serialize: bool = False) -> pd.DataF
     plant_investment_cycles = read_pickle_folder(
         intermediate_path, "investment_cycle_ref_result", "df"
     )
+    plant_investment_cycles_c = plant_investment_cycles.reset_index().set_index(["year", "plant_name"]).sort_values(["year"])
     plant_result_df = read_pickle_folder(
         intermediate_path, "plant_result_df", "df"
     )
-    plant_names_and_country_codes = zip(
-        plant_result_df["plant_name"].values, plant_result_df["country_code"].values
-    )
+    plant_names = plant_result_df["plant_name"].unique()
+    country_codes = plant_result_df["country_code"].unique()
+    technologies = SWITCH_DICT.keys()
     production_resource_usage = read_pickle_folder(
         final_path, "production_resource_usage", "df"
     )
     capex_df = create_capex_dict()
     max_year = max([int(year) for year in tech_choice_dict])
     year_range = range(MODEL_YEAR_START, max_year + 1)
-    data_container = []
-    for plant_name, country_code in tqdm(
-        plant_names_and_country_codes,
-        total=len(plant_result_df),
-        desc="Steel Plant Investments",
+
+
+    year_plant_product = list(itertools.product(year_range, plant_names))
+    year_plant_tech_product = list(itertools.product(year_range, plant_names, technologies))
+    year_country_code_plant_product = list(itertools.product(year_range, country_codes, plant_names))
+
+    capacity_ref = dict(zip(plant_result_df['plant_name'], plant_result_df['plant_capacity']))
+    plant_country_code_ref = dict(zip(plant_result_df['plant_name'], plant_result_df['country_code']))
+    
+    investment_switch_ref = {}
+    for year, plant_name in tqdm(
+        year_plant_product,
+        total=len(year_plant_product),
+        desc="Investment Reference Loop",
     ):
-        for year in year_range:
-            capacity_value = production_stats_getter(
-                production_resource_usage, year, plant_name, "capacity"
+        investment_switch_ref[(year, plant_name)] = plant_investment_cycles_c.loc[year, plant_name].values[0]
+
+    data_container = []
+    for year, plant_name in tqdm(
+        year_plant_product,
+        total=len(year_plant_product),
+        desc="Creating Steel Plant Investment DataFrame",
+    ):
+        data_container.append(
+            investment_row_calculator(
+                investment_switch_ref,
+                capex_df,
+                tech_choice_dict,
+                capacity_ref,
+                year,
+                plant_name
             )
-            data_container.append(
-                investment_row_calculator(
-                    plant_investment_cycles,
-                    capex_df,
-                    tech_choice_dict,
-                    plant_name,
-                    country_code,
-                    year,
-                    capacity_value,
-                )
-            )
+        )
 
     investment_results = (
         pd.DataFrame(data_container).set_index(["year"]).sort_values("year")
     )
+    country_ref = read_pickle_folder(PKL_DATA_IMPORTS, "country_ref", "df")
+    rmi_mapper = create_country_mapper(country_ref, 'rmi')
+    investment_results['country_code'] = investment_results['plant_name'].apply(
+        lambda x: plant_country_code_ref[x])
+    investment_results['rmi_region'] = investment_results['country_code'].apply(
+            lambda x: rmi_mapper[x])
     investment_results.reset_index(inplace=True)
     investment_results = map_plant_id_to_df(investment_results, plant_result_df, "plant_name")
     investment_results = add_results_metadata(
