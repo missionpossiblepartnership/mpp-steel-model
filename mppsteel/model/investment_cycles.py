dd32704448c6d2c4ebb952bcf2345af1277cef0d
drew-sys <90053966+drew-sys@users.noreply.github.com>
2022-04-09 07:40:40 +0000

    created a completely new optimization for investment by creating class

--- mppsteel/model/investment_cycles.py
+++ mppsteel/model/investment_cycles.py
@@ -1,6 +1,8 @@
 """Script to determine when investments will take place."""
 
 import random
+from typing import Tuple, Union
+
 import pandas as pd
 from tqdm import tqdm
 from copy import deepcopy
@@ -19,7 +21,7 @@ from mppsteel.config.model_config import (
 
 from mppsteel.utility.function_timer_utility import timer_func
 from mppsteel.utility.file_handling_utility import (
-    read_pickle_folder, serialize_file, get_scenario_pkl_path
+    read_pickle_folder, serialize_file
 )
 from mppsteel.utility.log_utility import get_logger
 
@@ -52,11 +54,36 @@ def calculate_investment_years(
         x += cycle_length
     return decision_years
 
+def convert_start_year(year_value: str):
+    if pd.isna(year_value):
+        return MODEL_YEAR_START
+    elif "(anticipated)" in str(year_value):
+        return int(year_value[:4])
+    else: 
+        try:
+            return int(float(year_value))
+        except:
+            return int(year_value[:4])
+
+def return_cycle_length(inv_intervals: int = INVESTMENT_CYCLE_DURATION_YEARS):
+    return inv_intervals + random.randrange(
+        -INVESTMENT_CYCLE_VARIANCE_YEARS, INVESTMENT_CYCLE_VARIANCE_YEARS, 1
+    )
+
+def return_switch_type(investment_cycle: list, year: int):
+    off_cycle_years = [inv_range for inv_range in investment_cycle if isinstance(inv_range, range)]
+    main_cycle_years = [inv_year for inv_year in investment_cycle if isinstance(inv_year, int)]
+    if year in main_cycle_years:
+        return 'main cycle'
+    for range_object in off_cycle_years:
+        if year in range_object:
+            return "trans switch"
+    return 'no switch'
 
 def add_off_cycle_investment_years(
     main_investment_cycle: list,
-    start_buff: int,
-    end_buff: int,
+    start_buff: int = INVESTMENT_OFFCYCLE_BUFFER_TOP,
+    end_buff: int = INVESTMENT_OFFCYCLE_BUFFER_TAIL,
 ) -> list:
     """Adds a set of off-cycle investment years to an investment decision list.
 
@@ -105,33 +132,7 @@ def add_off_cycle_investment_years(
 
     return range_list
 
-
-def apply_investment_years(row, cycle_length_mapper: dict) -> list:
-    """Formats the operating start date column values from and applies the calculate_investment_years function.
-
-    Args:
-        row (int): The raw operating start date column from plant data.
-
-    Returns:
-        list: The list of investment decision years.
-    """
-    year_value = row.start_of_operation
-    plant_cycle_length = cycle_length_mapper[row.plant_name]
-    if pd.isna(year_value):
-        return calculate_investment_years(MODEL_YEAR_START, plant_cycle_length)
-    elif "(anticipated)" in str(year_value):
-        year_value = year_value[:4]
-        return calculate_investment_years(int(year_value), plant_cycle_length)
-    else:
-        try:
-            return calculate_investment_years(int(float(year_value)), plant_cycle_length)
-        except:
-            return calculate_investment_years(int(year_value[:4]), plant_cycle_length)
-
-
-def create_investment_cycle_reference(
-    plant_names: list, investment_years: list, year_end: int
-) -> pd.DataFrame:
+def create_investment_cycle_reference(plant_investment_year_dict: dict) -> pd.DataFrame:
     """Creates an Investment cycle DataFrame from a plant DataFrame, and a list of main cycle and off-cycle investment years.
 
     Args:
@@ -142,96 +143,121 @@ def create_investment_cycle_reference(
     Returns:
         pd.DataFrame: _description_
     """
-    logger.info("Creating the investment cycle reference table")
-    zipped_plant_investments = zip(plant_names, investment_years)
-    year_range = range(MODEL_YEAR_START, year_end + 1)
-    df = pd.DataFrame(columns=["plant_name", "year", "switch_type"])
-
-    for plant_name, investments in tqdm(
-        zipped_plant_investments, total=len(plant_names), desc="Investment Cycles"
-    ):
+    df_list = []
+    year_range = range(MODEL_YEAR_START, MODEL_YEAR_END + 1)
+    for plant_name, investment_cycle in tqdm(plant_investment_year_dict.items(), total=len(plant_investment_year_dict), desc='Investment Cycle Ref'):
+        off_cycle_years = [inv_range for inv_range in investment_cycle if isinstance(inv_range, range)]
+        main_cycle_years = [inv_year for inv_year in investment_cycle if isinstance(inv_year, int)]
         for year in year_range:
-            row_dict = {
-                "plant_name": plant_name,
-                "year": year,
-                "switch_type": "no switch",
-            }
-            if year in [
-                inv_year for inv_year in investments if isinstance(inv_year, int)
-            ]:
-                row_dict["switch_type"] = "main cycle"
-            for range_object in [
-                inv_range for inv_range in investments if isinstance(inv_range, range)
-            ]:
-                if year in range_object:
-                    row_dict["switch_type"] = "trans switch"
-            df = df.append(row_dict, ignore_index=True)
-    return df.set_index(["year", "switch_type"])
+            if year in main_cycle_years:
+                entry = {'plant_name': plant_name, 'year': year, 'switch_type': 'main cycle'}
+                df_list.append(entry) 
+            elif any([True if year in range_object else False for range_object in off_cycle_years]):
+                    entry = {'plant_name': plant_name, 'year': year, 'switch_type': 'trans switch'}
+                    df_list.append(entry)
+            else:
+                entry = {'plant_name': plant_name, 'year': year, 'switch_type': 'no switch'}
+                df_list.append(entry)
+    return pd.DataFrame(df_list).set_index(["year", "plant_name"])
 
-
-def generate_investment_cycle_lengths(steel_plant_df: pd.DataFrame, inv_intervals: int = INVESTMENT_CYCLE_DURATION_YEARS) -> pd.DataFrame:
-    def return_cycle_length():
-        return inv_intervals + random.randrange(
-            -INVESTMENT_CYCLE_VARIANCE_YEARS, INVESTMENT_CYCLE_VARIANCE_YEARS, 1
-        )
-
-    investment_cycle_lengths = [return_cycle_length() for _ in range(len(steel_plant_df["plant_name"]))]
-
-    return dict(zip(steel_plant_df["plant_name"], investment_cycle_lengths))
-
-def create_investment_cycle(steel_plant_df: pd.DataFrame) -> pd.DataFrame:
-    """Full flow to create the investment decision cycle reference DataFrame.
+def extract_tech_plant_switchers(
+    plant_investment_year_dict: pd.DataFrame, year: int) -> Union[list, Tuple[list, list]]:
+    """Extracts the list of plants that are due for a main cycle switch or a transitional switch in a given year according to an investment cycle DataFrame.
 
     Args:
-        steel_plant_df (pd.DataFrame): Steel Plant DataFrame.
+        inv_cycle_ref (pd.DataFrame): DataFrame containing the investment cycle reference for each plant.
+        year (int): The year to extract the plant switchers for.
+        combined_output (bool, optional): Boolean switch that determines whether to return a combined list of switching plants or a tuple of two lists. Defaults to True.
 
     Returns:
-        pd.DataFrame: A DataFrame containing the Complete Investment Decision Cycle Reference.
+        Union[list, Tuple[list, list]]: Returns a single list of main cycle switchers and transitional switchers if `combined_output` if set to True, else a tuple of the two lists.
     """
-    logger.info("Creating investment cycle")
+    main_cycle_switchers = []
+    trans_cycle_switchers = []
+    non_switchers = []
+    for plant_name, investment_cycle in tqdm(plant_investment_year_dict.items(), total=len(plant_investment_year_dict), desc='Investment Cycle Ref'):
+        off_cycle_years = [inv_range for inv_range in investment_cycle if isinstance(inv_range, range)]
+        main_cycle_years = [inv_year for inv_year in investment_cycle if isinstance(inv_year, int)]
+        if year in main_cycle_years:
+            main_cycle_switchers.append(plant_name)
+        for range_object in off_cycle_years:
+            if year in range_object:
+                trans_cycle_switchers.append(plant_name)
+    combined_switchers = main_cycle_switchers + trans_cycle_switchers
+    for plant_name in plant_investment_year_dict:
+        if plant_name not in combined_switchers:
+            non_switchers.append(plant_name)
+    return main_cycle_switchers, trans_cycle_switchers, non_switchers, combined_switchers
 
-    plant_cycle_length_mapper = generate_investment_cycle_lengths(steel_plant_df)
-    investment_years = steel_plant_df.apply(
-        lambda row: apply_investment_years(row, plant_cycle_length_mapper), axis=1,
-    )
-    investment_years_inc_off_cycle = deepcopy(investment_years)
-    if len(investment_years) > 0:
-        investment_years_inc_off_cycle = [
-            add_off_cycle_investment_years(
-                inv_year, INVESTMENT_OFFCYCLE_BUFFER_TOP, INVESTMENT_OFFCYCLE_BUFFER_TAIL
-            )
-            for inv_year in investment_years
-        ]
-    investment_dict = dict(zip(steel_plant_df["plant_name"].values, investment_years_inc_off_cycle))
-
-    investment_df = create_investment_cycle_reference(
-        steel_plant_df["plant_name"].values,
-        investment_years_inc_off_cycle,
-        MODEL_YEAR_END,
-    )
-    return {'investment_df': investment_df, 'investment_dict': investment_dict, 'plant_cycle_length_mapper': plant_cycle_length_mapper}
-
-def amend_investment_dict(inv_dict: dict, plant: str, year: int):
-    inv_dict_c = deepcopy(inv_dict)
-    initial_list = inv_dict_c[plant]
-    inv_ranges_list = [year_obj for year_obj in initial_list if isinstance(year_obj, range)]
-    if len(inv_ranges_list) == 0:
-        return inv_dict_c
-    inv_range_match = [matching_range for matching_range in inv_ranges_list if year in matching_range]
-    if len(inv_range_match) == 0:
-        return inv_dict_c
+def adjust_investment_cycle_dict(cycle_years: list, rebase_year: int):
+    inv_ranges_list = [year_obj for year_obj in cycle_years if isinstance(year_obj, range)]
+    if not inv_ranges_list:
+        return cycle_years
+    inv_range_match = [matching_range for matching_range in inv_ranges_list if rebase_year in matching_range]
+    if not inv_range_match:
+        return cycle_years
     matching_range = inv_range_match[0]
-    index_position = initial_list.index(matching_range)
-    range_start = list(matching_range)[0]
-    new_range = range(range_start, year)
-    new_list = deepcopy(initial_list)
-    new_list[index_position] = new_range
-    inv_dict_c[plant] = new_list
-    return inv_dict_c
+    index_position = cycle_years.index(matching_range)
+    new_list = deepcopy(cycle_years)
+    new_list[index_position] = range(list(matching_range)[0], rebase_year)
+    return new_list
 
+class PlantInvestmentCycle():
+    def __init__(self):
+        self.plant_names = []
+        self.plant_start_years = {}
+        self.plant_investment_cycle_length = {}
+        self.plant_cycles = {}
+        self.plant_cycles_with_off_cycle = {}
+        
+    def instantiate_plants(self, plant_names: list, plant_start_years: list):
+        self.plant_names = plant_names
+        start_year_dict = dict(zip(plant_names, plant_start_years))
+        for plant_name in self.plant_names:
+            self.plant_start_years[plant_name] = convert_start_year(start_year_dict[plant_name])
+            self.plant_investment_cycle_length[plant_name] = return_cycle_length()
+            self.plant_cycles[plant_name] = calculate_investment_years(self.plant_start_years[plant_name], self.plant_investment_cycle_length[plant_name])
+            self.plant_cycles_with_off_cycle[plant_name] = add_off_cycle_investment_years(self.plant_cycles[plant_name])
+    
+    def add_new_plants(self, plant_names: list, plant_start_years: list):
+        new_dict = dict(zip(plant_names, plant_start_years))
+        for plant_name in plant_names:
+            self.plant_names.append(plant_name)
+            self.plant_start_years[plant_name] = new_dict[plant_name]
+            self.plant_investment_cycle_length[plant_name] = return_cycle_length()
+            self.plant_cycles[plant_name] = calculate_investment_years(self.plant_start_years[plant_name], self.plant_investment_cycle_length[plant_name])
+            self.plant_cycles_with_off_cycle[plant_name] = add_off_cycle_investment_years(self.plant_cycles[plant_name])
+            
+    def adjust_cycle_for_transitional_switch(self, plant_name: str, rebase_year: int):
+        new_cycle = adjust_investment_cycle_dict(self.plant_cycles_with_off_cycle[plant_name], rebase_year)
+        self.plant_cycles_with_off_cycle[plant_name] = new_cycle
+            
+    def create_investment_df(self):
+        return create_investment_cycle_reference(self.plant_cycles_with_off_cycle)
+
+    def return_plant_switch_type(self, plant_name: str, year: int):
+        return return_switch_type(self.plant_cycles_with_off_cycle[plant_name], year)
+    
+    def return_investment_dict(self):
+        return self.plant_cycles_with_off_cycle
+    
+    def return_cycle_lengths(self):
+        return self.plant_investment_cycle_length
+
+    def return_plant_switchers(self, year: int, value_type: str):
+        main_cycle_switchers, trans_cycle_switchers, non_switchers, combined_switchers = extract_tech_plant_switchers(self.plant_cycles_with_off_cycle, year)
+        if value_type == 'main cycle':
+            return main_cycle_switchers
+        elif value_type == 'trans switch':
+            return trans_cycle_switchers
+        elif value_type == 'no switch':
+            return non_switchers
+        elif value_type == 'combined':
+            return combined_switchers
+        
 
 @timer_func
-def investment_cycle_flow(scenario_dict: dict = None, serialize: bool = False) -> pd.DataFrame:
+def investment_cycle_flow(serialize: bool = False) -> pd.DataFrame:
     """Inintiates the complete investment cycle flow and serializes the resulting DataFrame.
 
     Args:
@@ -240,20 +266,18 @@ def investment_cycle_flow(scenario_dict: dict = None, serialize: bool = False) -
     Returns:
         pd.DataFrame: A DataFrame containing the Complete Investment Decision Cycle Reference.
     """
-    steel_plants_aug = read_pickle_folder(
+    steel_plant_df = read_pickle_folder(
         PKL_DATA_FORMATTED, "steel_plants_processed", "df"
     )
-    investment_object = create_investment_cycle(steel_plants_aug)
+
+    PlantInvestmentCycles = PlantInvestmentCycle()
+    steel_plant_names = steel_plant_df['plant_name'].to_list()
+    start_plant_years = steel_plant_df['start_of_operation'].to_list()
+    PlantInvestmentCycles.instantiate_plants(steel_plant_names, start_plant_years)
 
     if serialize:
         logger.info("-- Serializing Investment Cycle Reference")
         serialize_file(
-            investment_object['investment_df'], PKL_DATA_FORMATTED, "plant_investment_cycles"
+            PlantInvestmentCycles, PKL_DATA_FORMATTED, "plant_investment_cycle_container"
         )
-        serialize_file(
-            investment_object['investment_dict'], PKL_DATA_FORMATTED, "investment_dict"
-        )
-        serialize_file(
-            investment_object['plant_cycle_length_mapper'], PKL_DATA_FORMATTED, "plant_cycle_length_mapper"
-        )
-    return {'investment_df': investment_object['investment_df'], 'investment_dict': investment_object['investment_dict'], 'plant_cycle_length_mapper': investment_object['investment_dict']}
+    return PlantInvestmentCycles
