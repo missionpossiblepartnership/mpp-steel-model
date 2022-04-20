2526eeede84cdaefc1b81a9bc04307d613ca166c
drew-sys <90053966+drew-sys@users.noreply.github.com>
2022-04-20 17:26:37 +0000

    added new df_test script

---
+++ mppsteel/tests/df_tests.py
@@ -0,0 +1,9 @@
+"""Tests for negative values"""
+
+import pandas as pd
+
+def test_negative_df_values(df_combined: pd.DataFrame):
+    assert (df_combined.values < 0).any() == False
+
+def test_negative_list_values(a_list: list):
+    assert ( all( x > 0 for x in a_list ) )
