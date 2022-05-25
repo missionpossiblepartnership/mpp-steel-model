7d74c6a7b7c1474cc9b3a8384b2c31118e397107
Jochen Wersdörfer <jochen@wersdoerfer.de>
2022-05-25 17:08:21 +0200

    #169 make test result unambiguous

--- tests/dataframe_utility_test.py
+++ tests/dataframe_utility_test.py
@@ -7,5 +7,5 @@ import pandas as pd
 
 def test_move_cols_to_front():
     test_df = pd.DataFrame([(1, 2, 3), (4, 5, 6)], columns=["A", "B", "C"])
-    test_df_mod = move_cols_to_front(test_df, ["C"])
+    test_df_mod = move_cols_to_front(test_df, ["C", "A"])
     assert test_df_mod == ["C", "A", "B"]
