1793446417b0b97986e3af63c60a2246c71c6d78
cz-siq <104828540+cz-siq@users.noreply.github.com>
2022-06-01 11:27:54 +0200

    #174 Open coverage report on WSL

--- commands.py
+++ commands.py
@@ -1,3 +1,4 @@
+import platform
 import subprocess
 from pathlib import Path
 
@@ -24,9 +25,12 @@ def coverage():
     """
     Run and show coverage.
     """
-    subprocess.call(["coverage", "run"])
+    subprocess.call(["coverage", "run", "-m", "pytest"])
     subprocess.call(["coverage", "html"])
-    subprocess.call(["open", "htmlcov/index.html"])
+    if platform.system() == "Darwin":
+        subprocess.call(["open", "htmlcov/index.html"])
+    elif platform.system() == "Linux" and "Microsoft" in platform.release():  # on WSL
+        subprocess.call(["explorer.exe", r"htmlcov\index.html"])
 
 
 if __name__ == "__main__":
