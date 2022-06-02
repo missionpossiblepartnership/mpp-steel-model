908e54568f575befa7db2a23116f589e50f4ffc9
Jochen Wersdörfer <jochen@wersdoerfer.de>
2022-06-02 11:14:34 +0200

    #188 draft test

--- tests/choose_technology_test.py
+++ tests/choose_technology_test.py
@@ -1,1 +1,19 @@
 import pytest
+
+from mppsteel.config.model_scenarios import DEFAULT_SCENARIO
+from mppsteel.config.model_config import USD_TO_EUR_CONVERSION_DEFAULT, PROJECT_PATH
+
+from mppsteel.model_solver.solver import choose_technology
+
+@pytest.fixture
+def scenario_dict():
+    scenario_dict = DEFAULT_SCENARIO.copy()
+    scenario_dict["usd_to_eur"] = USD_TO_EUR_CONVERSION_DEFAULT
+    scenario_dict["eur_to_usd"] = 1.0 / scenario_dict["usd_to_eur"]
+    return scenario_dict
+
+
+def test_choose_technology(scenario_dict):
+    foo = choose_technology(scenario_dict)
+    print(foo)
+    assert False
