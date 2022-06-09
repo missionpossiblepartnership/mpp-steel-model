64852166f45a8ad01b2a2167554dbb6e02accdbe
drew-sys <90053966+drew-sys@users.noreply.github.com>
2022-06-09 16:14:42 +0000

    turned off regional scrap

--- mppsteel/config/model_scenarios.py
+++ mppsteel/config/model_scenarios.py
@@ -85,7 +85,7 @@ DEFAULT_SCENARIO = {
     "tech_switch_scenario": "lowest_cost",  # max_abatement / lowest_cost / equal_weight
     "solver_logic": "scale",  # scale / rank / bins
     "trade_active": True,  # bool
-    "regional_scrap_constraint": True,  # bool
+    "regional_scrap_constraint": False,  # bool
 }
 TECH_MORATORIUM = {
     "scenario_name": "tech_moratorium",
@@ -105,7 +105,7 @@ TECH_MORATORIUM = {
     "tech_switch_scenario": "lowest_cost",
     "solver_logic": "rank",
     "trade_active": True,
-    "regional_scrap_constraint": True,
+    "regional_scrap_constraint": False,
 }
 CARBON_COST = {
     "scenario_name": "carbon_cost",
@@ -125,7 +125,7 @@ CARBON_COST = {
     "tech_switch_scenario": "lowest_cost",
     "solver_logic": "rank",
     "trade_active": True,
-    "regional_scrap_constraint": True,
+    "regional_scrap_constraint": False,
 }
 BAU_SCENARIO = {
     "scenario_name": "baseline",
@@ -145,7 +145,7 @@ BAU_SCENARIO = {
     "tech_switch_scenario": "lowest_cost",
     "solver_logic": "rank",
     "trade_active": True,
-    "regional_scrap_constraint": True,
+    "regional_scrap_constraint": False,
 }
 BAU_HIGH_CIRC_SCENARIO = {
     "scenario_name": "baseline_high_circ",
@@ -165,7 +165,7 @@ BAU_HIGH_CIRC_SCENARIO = {
     "tech_switch_scenario": "lowest_cost",
     "solver_logic": "rank",
     "trade_active": True,
-    "regional_scrap_constraint": True,
+    "regional_scrap_constraint": False,
 }
 ABATEMENT_SCENARIO = {
     "scenario_name": "abatement",
@@ -185,7 +185,7 @@ ABATEMENT_SCENARIO = {
     "tech_switch_scenario": "max_abatement",
     "solver_logic": "rank",
     "trade_active": True,
-    "regional_scrap_constraint": True,
+    "regional_scrap_constraint": False,
 }
 ABATEMENT_HIGH_CIRC_SCENARIO = {
     "scenario_name": "fastest_abatement",
@@ -205,7 +205,7 @@ ABATEMENT_HIGH_CIRC_SCENARIO = {
     "tech_switch_scenario": "max_abatement",
     "solver_logic": "rank",
     "trade_active": True,
-    "regional_scrap_constraint": True,
+    "regional_scrap_constraint": False,
 }
 
 SCENARIO_OPTIONS = {
