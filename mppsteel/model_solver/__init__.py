6a60fb469a32845579186aa880ff6104a40b4374
drew-sys <90053966+drew-sys@users.noreply.github.com>
2022-07-15 13:16:52 +0000

    updates to inits files

--- mppsteel/model_solver/__init__.py
+++ mppsteel/model_solver/__init__.py
@@ -1,7 +1,8 @@
 from . import (
+    plant_open_close_flow,
     solver,
     tco_and_abatement_optimizer,
-    plant_open_close,
+    plant_open_close_helpers,
     solver_classes,
     solver_constraints,
 )
--- mppsteel/trade_module/__init__.py
+++ mppsteel/trade_module/__init__.py
@@ -1,6 +1,8 @@
 from . import (
     trade_flow,
+    trade_flow_test,
+    trade_helpers_test,
+    trade_logic_test,
     trade_logic,
-    trade_helpers,
-    trade_test
+    trade_helpers
 )
